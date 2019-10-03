0334c73f735ebddc3145a3d617fcffe782e5e6e3
igotapochahontas <igotapochahontas@yahoo.com>
2019-10-02 20:53:17 -0500

    Add files via upload

---
+++ code/search.js
@@ -0,0 +1,22 @@
+var http = require("http")
+var console = require("console")
+module.exports.function = function search (day, cam) {
+  var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?"
+var urlFull= "earth_date=2015-6-3&api_key=DEMO_KEY"
+  var options = {}
+  var ret = []
+  if (typeof(day)!= "undefined"){
+    options["day"]= day
+  }
+  if (typeof(cam)!= "undefined"){
+    options["cam"]= cam
+  }
+  
+  try{
+  res = http.getUrl(url,{query: options})
+  ret = JSON.parse(res)
+  } catch (err){
+    console.log("ERROR: "+err)
+  }
+  return  ret
+}
